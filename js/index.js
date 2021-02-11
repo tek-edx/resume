8c16f761a28493fbe74a7e3951e2a3dcc5f9eab6
Tek <edx.tek@gmail.com>
2021-02-11 18:53:15 +1100

    Fix the hamburger menu bug , scroll problem when click on nav

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@ const burgBox = document.querySelector('.burger-box');
 let menuClk = false;
 
 document.getElementById('nav-menu').style.display = 'none';
+document.querySelector('#nav-menu ul').addEventListener('click',() => {setScreenOff()});
 
 burgBox.addEventListener('click',(e) => {
     e.preventDefault();
@@ -14,11 +15,11 @@ burgBox.addEventListener('click',(e) => {
         console.log(`burger activated ${menuClk}`)
         burgBox.classList.add('open');
         document.getElementById('nav-menu').style.display='block';
-        document.body.style.overflowY="hidden";
+         document.body.style.overflowY="hidden";
         menuClk = true;
         
     }else {
-        console.log(`burger activated ${menuClk}`)
+       
          burgBox.classList.remove('open');
          document.getElementById('nav-menu').style.display='none';
         document.body.style.overflowY="visible";
@@ -31,3 +32,9 @@ burgBox.addEventListener('click',(e) => {
 
 /* ---------Javascript for burger menu ended ----------*/
 
+function setScreenOff(){
+    burgBox.classList.remove('open');
+    document.getElementById('nav-menu').style.display = 'none';
+    document.body.style.overflowY = "visible";
+    menuClk = false;
+}
